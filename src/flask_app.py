c842c07833db28013ac5e5b24176910872e17e23
Yisu Luo <lys93512@gmail.com>
2024-03-29 12:15:36 -0400

    1. Add dependency page draft 2. Add relevant backend interface and json encoder

---
+++ src/flask_app.py
@@ -0,0 +1,66 @@
+import json
+
+from flask import Flask, jsonify, request, make_response, send_file
+
+from database_connection import DatabaseManager
+from src.json_encoder import JSONEncoder
+
+app = Flask(__name__)
+
+
+@app.route('/test')
+def test():
+    filename = '/Users/luoyisu/Downloads/APP/echarts-5.5.0/dist/test.json'
+    return send_file(filename, mimetype='application/json')
+
+
+@app.route('/get_repository')
+def get_repository():
+    full_name = request.args.get('full_name')
+    connection = DatabaseManager('3.139.100.241', 27017)
+    connection.connect_mongo("test_database", "test_collection")
+    query = {"full_name": full_name}
+    result = list(connection.find(query).limit(1))
+    if not result:
+        return make_response(jsonify({"error": "Content not found"}), 404)
+
+    return jsonify(json.loads(json.dumps(result, cls=JSONEncoder)))
+
+
+@app.route('/get_dependency')
+def get_dependency():
+    full_name = request.args.get('full_name')
+    connection = DatabaseManager('3.139.100.241', 27017)
+    connection.connect_mongo("test_database", "test_collection")
+    query = {"full_name": full_name}
+    result = list(connection.find(query).limit(1))
+    if not result:
+        return make_response(jsonify({"error": "Content not found"}), 404)
+
+    result = result[0]
+
+    # modify the json to fit Echarts
+    children = []
+    for dependency in result['dependency_project_id']:
+        child = {
+            "name": dependency.split('/')[-1],
+            "full_name": dependency,
+            "children": []
+        }
+        children.append(child)
+    result['children'] = children
+    result['apiCalled'] = True
+
+    return jsonify(json.loads(json.dumps(result, cls=JSONEncoder)))
+
+
+@app.after_request
+def after_request_func(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
---
+++ src/json_encoder.py
@@ -0,0 +1,17 @@
+import json
+from bson import ObjectId
+from datetime import datetime
+
+class JSONEncoder(json.JSONEncoder):
+    """
+        Extended JSONEncoder for converting ObjectId and datetime to str.
+
+        Use like this: json.dumps(document, cls=JSONEncoder)
+    """
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        elif isinstance(o, datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
+
