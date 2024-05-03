adeec2250dbd81542014ed293c7b06746b481dc1
yaotiancq <125851962+yaotiancq@users.noreply.github.com>
2024-05-03 12:57:23 -0400

    Add files via upload

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,4 +1,6 @@
+import pymongo
 from pymongo import MongoClient
+import datetime
 
 
 class DatabaseManager:
@@ -31,36 +33,24 @@ class DatabaseManager:
         It returns a cursor object. You need to iterate over the cursor to get the documents."""
         return self.collection.find(query)
     
-    def find(self, d=None, limit=-1, sort_by=None, sort_order=-1, skip=0, sort_by_array_size=None):
+    def find_all(self, d=None, limit=-1, selected_field=[], sort_by=None, skip=0):
         """Find documents in the collection, if query is not provided, it will return all documents.
         It returns a list of dictionaries."""
-        pipeline = []
-
+        projection = {}
+        projection['_id'] = 0
+        if selected_field:
+            for field in selected_field:
+                projection[field] = 1
         if d:
-            pipeline.append({"$match": d})
-
-        # add the size of the sorted field
-        if sort_by_array_size:
-            pipeline.append({
-                "$addFields": {
-                    "count_for_sort": {"$size": f"${sort_by_array_size}"}
-                }
-            })
-
-        # Sort the results
+            cursor = self.collection.find(d, projection)
+        else:
+            cursor = self.collection.find({}, projection)
         if sort_by:
-            pipeline.append({"$sort": {sort_by: sort_order}})
-        elif sort_by_array_size:
-            pipeline.append({"$sort": {"count_for_sort": sort_order}})
-
-        # Apply skip and limit
+            cursor = cursor.sort(sort_by,-1)
         if skip > 0:
-            pipeline.append({"$skip": skip})
+            cursor = cursor.skip(skip)
         if limit > 0:
-            pipeline.append({"$limit": limit})
-
-        # Execute the aggregation pipeline
-        cursor = self.collection.aggregate(pipeline)
+            cursor = cursor.limit(limit)
         return list(cursor)
           
     def _count(self, query):
@@ -95,10 +85,18 @@ class DatabaseManager:
 
     def _update_all_field(self, query, new_query):
         """Update the target document"""
-        self.collection.update(query, new_query)
+        self.collection.update_one(query, new_query)
 
-    def search_bar(self, keyword=None, language=None, project_license=None, limit_num=-1, category=None, sort_order=-1,
-                   page=1, entryNum=10):
+    def update(self, keyword, d):
+        """Update the document based on the keyword, d is a dictionary that contains the new values.
+        It will update the first document that matches the keyword.
+        keyword: the field 'name' in the document
+        d: a dictionary that contains the new values, it will update all fields in the document."""
+        query = {'name': keyword}
+        update_operation = {'$set': d}
+        self._update_all_field(query, update_operation)
+
+    def search_bar(self, keyword=None, language=None, license=None, limit_num = -1, category=None, page=1, entryNum=10):
         """Search the collection based on the keyword, language, license, and category.
         It returns a list of dictionaries, also returns the total number of documents.
         If num is provided, it will return the number of documents specified by num.
@@ -109,29 +107,17 @@ class DatabaseManager:
         skip_row = (page - 1) * entry_per_page
         if language:
             query['language'] = language
-        if project_license:
-            query['project_license'] = {"$regex": project_license, "$options": "i"}
+        if license:
+            query['project_license'] = license
         if keyword:
-            query['full_name'] = {"$regex": keyword, "$options": "i"}
-        cnt = self._count(query)
-        if category:
-            if category == "Recent Updated":
-                ans = self.find(query, sort_by='updated_at', sort_order=-1, limit=limit_num, skip=skip_row)[
-                      :entry_per_page]
-            elif category == "Most Popular":
-                ans = self.find(query, sort_by='stars', sort_order=-1, limit=limit_num, skip=skip_row)[
-                      :entry_per_page]
-            elif category == "Least Dependencies":
-                ans = self.find(query, sort_by_array_size='dependency_project_id', sort_order=1, limit=limit_num,
-                                skip=skip_row)[:entry_per_page]
-        else:
-            ans = self.find(query, sort_by=category, sort_order=sort_order, limit=limit_num, skip=skip_row)[
-                  :entry_per_page]
+            query['name'] = {"$regex": keyword, "$options": "i"}
+        cnt=self._count(query)
+        ans=self.find_all(query, sort_by = category, limit=limit_num ,skip = skip_row)[:entry_per_page]
         return ans, cnt
         
 def insertion_test():
     data=[{'project_id': 10000, 
-           'name': 'xkcd2347', 'full_name': 'tom1', 'owner_id': 33829, 
+           'name': 'aaa', 'full_name': 'tom1', 'owner_id': 33829, 
            'owner_name': 'edsu', 'dependency_project_id': ['Baughn/python-diskcache', 'pytest-dev/pytest', 'theskumar/python-dotenv', 'yaml/pyyaml', 'psf/requests'], 
            'releases': [], 'description': 'Get dependencies for a project on GitHub.', 
            'project_license': 'MIT License', 'html_url': 'https://github.com/edsu/xkcd2347', 
@@ -140,7 +126,7 @@ def insertion_test():
            'updated_at': '2024-01-18T09:05:33Z', 'pushed_at': '2021-06-17T17:32:26Z', 
            'created_at': '2020-08T17:32:26Z', 'disabled': False},
            {'project_id': 20000, 
-           'name': 'xkcd2347', 'full_name': 'jack2', 'owner_id': 33829, 
+           'name': 'bbb', 'full_name': 'jack2', 'owner_id': 33829, 
            'owner_name': 'edsu', 'dependency_project_id': ['Baughn/python-diskcache', 'pytest-dev/pytest', 'theskumar/python-dotenv', 'yaml/pyyaml', 'psf/requests'], 
            'releases': [], 'description': 'Get dependencies for a project on GitHub.', 
            'project_license': 'MIT License', 'html_url': 'https://github.com/edsu/xkcd2347', 
@@ -149,7 +135,7 @@ def insertion_test():
            'updated_at': '2024-01-18T09:05:33Z', 'pushed_at': '2021-06-17T17:32:26Z', 
            'created_at': '2020-08T17:32:26Z', 'disabled': False},
            {'project_id': 30000, 
-           'name': 'xkcd2347', 'full_name': 'lily3', 'owner_id': 33829, 
+           'name': 'ccc', 'full_name': 'lily3', 'owner_id': 33829, 
            'owner_name': 'edsu', 'dependency_project_id': ['Baughn/python-diskcache', 'pytest-dev/pytest', 'theskumar/python-dotenv', 'yaml/pyyaml', 'psf/requests'], 
            'releases': [], 'description': 'Get dependencies for a project on GitHub.', 
            'project_license': 'MIT License', 'html_url': 'https://github.com/edsu/xkcd2347', 
@@ -158,27 +144,32 @@ def insertion_test():
            'updated_at': '2024-01-18T09:05:33Z', 'pushed_at': '2021-06-17T17:32:26Z', 
            'created_at': '2020-08T17:32:26Z', 'disabled': False}
            ]
-    db=DatabaseManager('3.139.100.241', 27017)
+
+    db=DatabaseManager('3.139.100.241', 27018)
     db.connect_mongo("just_for_test", "just_for_test")
     db.insert(data)
 
 def search_test():
+    connection = DatabaseManager('3.139.100.241', 27018)
+    connection.connect_mongo("just_for_test", "just_for_test")
+    #print(connection.search_bar(keyword='aaa',category='stars',page=1, entryNum=10)[0])
+    print(connection.find_all({'name':'aaa'}))
 
-    connection = DatabaseManager('3.139.100.241', 27017)
-    connection.connect_mongo("test_database", "test_collection")
+def update_test():
+    connection = DatabaseManager('3.139.100.241', 27018)
+    connection.connect_mongo("just_for_test", "just_for_test")
+    connection.update('aaa', {'stars': 40000})
 
-    # pagaination test
-    # page=1
-    # entryNum=10
-    # res=connection.search_bar(category='stars',page=page, entryNum=entryNum)[0]
-    # total=connection.search_bar(category='stars',page=page, entryNum=entryNum)[1]
-    # for i in range(len(res)):
-    #     print(res[i],'\t', (page-1)*entryNum+i+1,'/',total)
+def remove_test():
+    connection = DatabaseManager('3.139.100.241', 27018)
+    connection.connect_mongo("just_for_test", "just_for_test")
+    connection._remove()
 
-    # fuzzy search test
-    print(connection.search_bar(keyword='dj',category='stars',page=1, entryNum=10)[0])
-    
 
 if __name__ == "__main__":
+    # insertion_test()
+    # remove_test()
+    search_test()
+    update_test()
     search_test()
     
